067d2f75111d70ffe9aab21e81a048a2c011a49f
Jeff Wilgus <jeff@fortnine.ca>
2025-04-25 09:45:22 -0400

    expand and extract distinct maintenance

--- src/main.rs
+++ src/main.rs
@@ -14,20 +14,21 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    if let Some(file) = args.file {
-        for repo in read_to_string(file).unwrap().lines() {
-            run_gc_on(repo);
-        }
-    } else if let Some(repos) = args.repos {
-        for repo in repos {
-            run_gc_on(&repo);
-        }
-    } else { println!("No file specified"); }
+    for repo in args.repos {
+        do_maintenance_on(&repo);
+    }
+}
+
+fn do_maintenance_on(repo: &str) -> () {
+    let cwd = std::env::current_dir().unwrap();
+    set_current_dir(repo).expect(format!("No such directory: {repo}").as_str());
+    run_gc_on(repo);
+    prune_branches(repo);
+    print_stash_count_for(repo);
+    set_current_dir(cwd.as_path()).unwrap();
 }
 
 fn run_gc_on(repo: &str) -> () {
-    let cwd = std::env::current_dir().unwrap();
-    set_current_dir(repo).expect(format!("No such directory: {repo}").as_str());
     Command::new("git")
         .arg("gc")
         .arg("--aggressive")
@@ -35,5 +36,46 @@ fn run_gc_on(repo: &str) -> () {
         .stderr(Stdio::inherit())
         .output()
         .expect(format!("Failed to clean {repo}").as_str());
-    set_current_dir(cwd.as_path()).unwrap();
+}
+
+fn prune_branches(repo: &str) -> () {
+    Command::new("git")
+        .arg("fetch")
+        .arg("--prune")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .expect(format!("Failed to prune branches from {repo}").as_str());
+}
+
+fn print_stash_count_for(repo: &str) -> () {
+    let count = get_stash_count_for(repo);
+    if count > 0 {
+        println!("{}", get_stash_warning_message(repo, count));
+    }
+}
+
+fn get_stash_count_for(repo: &str) -> i32 {
+    let stash_cmd = Command::new("git")
+        .arg("stash")
+        .arg("list")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect(format!("Failed to run git stash list on {repo}").as_str());
+    let count = Command::new("wc")
+        .arg("-l")
+        .stdin(Stdio::from(stash_cmd.stdout.unwrap()))
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit())
+        .output()
+        .expect(format!("Failed to count stash entries on {repo}").as_str());
+    String::from_utf8(count.stdout).unwrap().trim().parse::<i32>().unwrap()
+}
+
+fn get_stash_warning_message(repo: &str, count: i32) -> String {
+    let yellow_repo = repo.yellow();
+    let yellow_count = count.to_string().yellow();
+    let stash_entry_str = if count == 1 { "stash entry" } else { "stash entries" };
+    format!("{} {} {} {}", yellow_repo, "has".yellow(), yellow_count, stash_entry_str.yellow())
 }
